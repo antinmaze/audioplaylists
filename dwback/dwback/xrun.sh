a8afe63498763b57d86a3b2d653f1e78168d3035
antinmaze <antinmaze@gmail.com>
2022-06-03 17:05:10 +0200

    Update xrun.sh
    correction upgrade Werkzeug version from 2.0.3 to 2.1.0
    https://github.com/django-extensions/django-extensions/issues/1715

--- dwback/dwback/xrun.sh
+++ dwback/dwback/xrun.sh
@@ -1,4 +1,4 @@
 #python manage.py makemigrations
 #python manage.py migrate
 export ENVIRONMENT="DEV"
-python manage.py runserver_plus --cert-file cert.pem --key-file key.pem
+python manage.py runserver_plus --cert-file cert.pem --key-file key.pem --keep-meta-shutdown
